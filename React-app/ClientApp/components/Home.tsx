078b71c96887fd7c4c928263c83ae801c234f9ee
texyh <onwuzulikee1@gmail.com>
2019-01-15 20:36:44 +0100

    made change

--- React-app/ClientApp/components/Home.tsx
+++ React-app/ClientApp/components/Home.tsx
@@ -12,7 +12,7 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
                 <li><a href='https://webpack.github.io/'>Webpack</a> for building and bundling client-side resources</li>
                 <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
             </ul>
-            <p>To help you get started, we've also set up:</p>
+            <p>To help you get started, we've also set up: HAYYYYYY</p>
             <ul>
                 <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
                 <li><strong>Webpack dev middleware</strong>. In development mode, there's no need to run the <code>webpack</code> build tool. Your client-side resources are dynamically built on demand. Updates are available as soon as you modify any file.</li>
